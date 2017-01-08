ec26f46847eb5ec304f8552fc878c8d64304c2e6
LucasDaiki <lucas.daiki1983@gmail.com>
2017-01-08 21:03:05 -0200

    wip

--- src/Scope.js
+++ src/Scope.js
@@ -14,5 +14,37 @@ module.exports = {
     };  
     xablau(x);  
     return x;
+  },
+  example3: function() {
+    console.log('example3(0) =>', this);    
+    
+    // const e = () => {
+    //   console.log('example3(1)', this);
+    // }
+
+    // function f() {
+    //   console.log('example3(2)', this);
+    // }
+
+    // e();
+    // f();
+
+    return this;
+  },
+  example4: () => {
+    // console.log('example(41) =>', this);    
+    
+    // const e = () => {
+    //   console.log('example4(1)', this);
+    // }
+
+    // function f() {
+    //   console.log('example4(2)', this);
+    // } 
+
+    // e();
+    // f();
+    
+    // return this;
   }
 }
--- test/Scope.test.js
+++ test/Scope.test.js
@@ -1,5 +1,5 @@
 const { expect } = require('chai');
-const { example1, example2 } = require('../src/Scope');
+const { example1, example2, example3, example4 } = require('../src/Scope');
 
 describe('Scope', () => {
   describe('#example1', () => {
@@ -13,6 +13,20 @@ describe('Scope', () => {
     it('must return 2', () => {
       const result = example2();
       expect(result).to.be.equal(1);
+    });
+  });
+
+  describe('#example3', () => {
+    it('WTF', () => {
+      const result = example3.call(this);
+      // console.log('example3 =>', result);
+    });
+  });
+
+  describe('#example4', () => {
+    it('WTF', () => {
+      const result = example4();
+      // console.log('example4 =>',result);
     });
   });
 });
